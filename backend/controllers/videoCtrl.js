88679b4f0743f7686e3bd12d454a2f29bb1c8e93
Wayne Zhang <waynezhangus@gmail.com>
2022-06-16 09:33:47 -0700

    backend error checking

--- backend/controllers/videoCtrl.js
+++ backend/controllers/videoCtrl.js
@@ -1,40 +1,49 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const Video = require('../models/videoModel');
-const blackRanges = require('./constants');
+const videosRanges = require('./constants');
 const { getCCKeywords, getComments } = require('./api');
 
 // @desc    Request new video info
 // @route   POST /api/youtube
 // @access  Private
 const addVideo = asyncHandler( async (req, res) => {
-
   const { videoId } = req.body;
-  const ccKeywords = await getCCKeywords(videoId)
-  const comments = await getComments(videoId)
+  const ccKeywords = await getCCKeywords(videoId);
+  const comments = await getComments(videoId);
+  const blackRanges = videosRanges[videoId] ?? [];
 
-  const video = Video({
-    videoId,
-    ccKeywords,
-    comments,
-    blackRanges, 
-    status: 'available',
-  })
-
-  await video.save()
-  res.status(201).json(video);
+  const video = await Video.findOne({videoId: videoId})
+  if (!video) {
+    const videoNew = Video({
+      videoId,
+      ccKeywords,
+      comments,
+      blackRanges, 
+      status: 'available',
+    })
+    videoNew.save()
+    res.status(201).json(videoNew);
+  } else {
+    video.ccKeywords = ccKeywords;
+    video.comments = comments;
+    video.blackRanges = blackRanges;
+    video.save()
+    res.status(200).json(video);
+  }
 })
 
 // @desc    Get one video info
 // @route   GET /api/youtube/:id
 // @access  Private
 const getVideo = asyncHandler( async (req, res) => {
-  const video = await Video.find({videoId: req.params.id})
-  if (video.length == 0) {
-    res.status(404)
-    throw new Error('Video not found')
-  } 
-  res.status(200).json(video[video.length - 1])
+  const video = await Video.findOne({videoId: req.params.id})
+  if (!video) {
+    res.status(404);
+    throw new Error('Video not found');
+  } else {
+    res.status(200).json(video);
+  }
 })
 
 const videoCtrl = {
