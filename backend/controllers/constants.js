386627fa66494292622892baea0d7c911f106962
Biao Wang <bw437@cornell.edu>
2022-06-29 02:10:24 -0700

    More data

--- backend/controllers/constants.js
+++ backend/controllers/constants.js
@@ -841,7 +841,8 @@ const videosRanges = {
       start: 64.3,
       end: 65.67,
       score: 0.2108,
-      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
     },
     {
       start: 65.67,
@@ -859,13 +860,15 @@ const videosRanges = {
       start: 92.23,
       end: 100.1,
       score: 0.3648,
-      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
     },
     {
       start: 100.1,
       end: 108.97,
       score: 0.3207,
-      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
     },
     {
       start: 108.97,
@@ -877,7 +880,8 @@ const videosRanges = {
       start: 115.23,
       end: 120.4,
       score: 0.2468,
-      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
     },
     {
       start: 120.4,
@@ -901,7 +905,8 @@ const videosRanges = {
       start: 133.63,
       end: 185.8,
       score: 0.2163,
-      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
     },
     {
       start: 185.8,
@@ -1254,12 +1259,712 @@ const videosRanges = {
       reason: 'specific visual details not covered by speech',
     },
   ],
-  // qPix_X-9t7E: ,
-  // lEXBxijQREo: ,
-  // FK3dav4bA4s: ,
-  // WzJc75S-LzY: ,
+  'qPix_X-9t7E': [
+    {
+      start: 0.0,
+      end: 68.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 68.0,
+      end: 78.0,
+      score: 0.4,
+      reason:
+        'contain no-speech segment | reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 78.0,
+      end: 101.0,
+      score: 1.0,
+      reason:
+        'contain no-speech segment | reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 101.0,
+      end: 106.0,
+      score: 0.4,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 106.0,
+      end: 197.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 197.0,
+      end: 203.0,
+      score: 0.1,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 203.0,
+      end: 246.0,
+      score: 1.0,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 246.0,
+      end: 253.0,
+      score: 0.4,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 253.0,
+      end: 257.0,
+      score: 1.0,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 257.0,
+      end: 313.0,
+      score: 0.4,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 313.0,
+      end: 385.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 385.0,
+      end: 410.0,
+      score: 0.4,
+      reason:
+        'contain no-speech segment | reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 410.0,
+      end: 438.0,
+      score: 1.0,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 438.0,
+      end: 445.0,
+      score: 0.1,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 445.0,
+      end: 451.0,
+      score: 0.1,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 451.0,
+      end: 454.0,
+      score: 1.0,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 454.0,
+      end: 460.0,
+      score: 0.1,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 460.0,
+      end: 516.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 516.0,
+      end: 546.0,
+      score: 0.1,
+      reason: 'frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 546.0,
+      end: 635.0,
+      score: 1.0,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+  ],
+  lEXBxijQREo: [
+    {
+      start: 0.0,
+      end: 6.0,
+      score: 0.4,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 6.0,
+      end: 15.0,
+      score: 1.0,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 15.0,
+      end: 17.0,
+      score: 0.1,
+      reason: 'frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 17.0,
+      end: 67.0,
+      score: 1.0,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 67.0,
+      end: 75.0,
+      score: 0.1,
+      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 75.0,
+      end: 86.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 86.0,
+      end: 102.0,
+      score: 0.1,
+      reason:
+        'reference words without explanations | on-screen text not in speech | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 102.0,
+      end: 106.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 106.0,
+      end: 110.0,
+      score: 0.4,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 110.0,
+      end: 138.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 138.0,
+      end: 150.0,
+      score: 0.4,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 150.0,
+      end: 152.0,
+      score: 1.0,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 152.0,
+      end: 156.0,
+      score: 0.4,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 156.0,
+      end: 157.0,
+      score: 1.0,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 157.0,
+      end: 167.0,
+      score: 0.4,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 167.0,
+      end: 169.0,
+      score: 1.0,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 169.0,
+      end: 176.0,
+      score: 0.1,
+      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 176.0,
+      end: 178.0,
+      score: 1.0,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 178.0,
+      end: 186.0,
+      score: 0.1,
+      reason: 'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 186.0,
+      end: 222.0,
+      score: 1.0,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 222.0,
+      end: 227.0,
+      score: 0.4,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 227.0,
+      end: 279.0,
+      score: 1.0,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 279.0,
+      end: 282.0,
+      score: 0.4,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 282.0,
+      end: 289.0,
+      score: 1.0,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 289.0,
+      end: 302.0,
+      score: 0.4,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+  ],
+  FK3dav4bA4s: [
+    {
+      start: 0.0,
+      end: 2.07,
+      score: 0.3974,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 2.07,
+      end: 5.91,
+      score: 0.5077,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 5.91,
+      end: 46.08,
+      score: 0.8763,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 46.08,
+      end: 53.95,
+      score: 0.5317,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 53.95,
+      end: 57.32,
+      score: 0.3845,
+      reason:
+        'on-screen text not in speech | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 57.32,
+      end: 75.94,
+      score: 0.6117,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 75.94,
+      end: 80.78,
+      score: 1.0,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 80.78,
+      end: 85.19,
+      score: 0.7846,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 85.19,
+      end: 93.39,
+      score: 0.415,
+      reason: 'frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 93.39,
+      end: 98.13,
+      score: 0.5394,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 98.13,
+      end: 105.47,
+      score: 0.4947,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 105.47,
+      end: 109.11,
+      score: 0.7087,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 109.11,
+      end: 133.37,
+      score: 0.6815,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 133.37,
+      end: 140.04,
+      score: 0.4187,
+      reason:
+        'contain no-speech segment | on-screen text not in speech | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 140.04,
+      end: 170.44,
+      score: 0.5066,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 170.44,
+      end: 172.31,
+      score: 0.1572,
+      reason: 'frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 172.31,
+      end: 176.48,
+      score: 0.3408,
+      reason:
+        'contain no-speech segment | on-screen text not in speech | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 176.48,
+      end: 181.51,
+      score: 0.0,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 181.51,
+      end: 183.42,
+      score: 0.0403,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 183.42,
+      end: 185.99,
+      score: 0.0639,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 185.99,
+      end: 188.72,
+      score: 0.0763,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 188.72,
+      end: 191.69,
+      score: 0.0467,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 191.69,
+      end: 194.76,
+      score: 0.07,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 194.76,
+      end: 196.43,
+      score: 0.0176,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 196.43,
+      end: 196.53,
+      score: 0.0292,
+      reason: 'specific visual details not covered by speech',
+    },
+  ],
+  'WzJc75S-LzY': [
+    {
+      start: 0.0,
+      end: 0.67,
+      score: 0.0487,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 0.67,
+      end: 7.14,
+      score: 0.4286,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 7.14,
+      end: 8.64,
+      score: 0.4309,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 8.64,
+      end: 14.01,
+      score: 0.2286,
+      reason:
+        'contain no-speech segment | reference words without explanations | ',
+    },
+    {
+      start: 14.01,
+      end: 15.58,
+      score: 0.4466,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 15.58,
+      end: 23.12,
+      score: 0.6779,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 23.12,
+      end: 24.66,
+      score: 0.6132,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 24.66,
+      end: 25.66,
+      score: 0.3238,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 25.66,
+      end: 26.66,
+      score: 0.0,
+      reason:
+        'contain no-speech segment | reference words without explanations | ',
+    },
+    {
+      start: 26.66,
+      end: 29.43,
+      score: 0.5476,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 29.43,
+      end: 44.81,
+      score: 0.6617,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 44.81,
+      end: 46.85,
+      score: 0.502,
+      reason:
+        'reference words without explanations | on-screen text not in speech | ',
+    },
+    {
+      start: 46.85,
+      end: 48.15,
+      score: 0.2525,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 48.15,
+      end: 49.15,
+      score: 0.013,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 49.15,
+      end: 50.65,
+      score: 0.2118,
+      reason:
+        'contain no-speech segment | reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 50.65,
+      end: 54.39,
+      score: 0.0297,
+      reason:
+        'contain no-speech segment | reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 54.39,
+      end: 60.99,
+      score: 0.4734,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 60.99,
+      end: 62.9,
+      score: 0.6229,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 62.9,
+      end: 65.06,
+      score: 0.5098,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 65.06,
+      end: 69.04,
+      score: 0.6592,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 69.04,
+      end: 70.2,
+      score: 0.8486,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 70.2,
+      end: 71.17,
+      score: 0.2921,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 71.17,
+      end: 72.2,
+      score: 0.5947,
+      reason: 'contain no-speech segment | ',
+    },
+    {
+      start: 72.2,
+      end: 74.17,
+      score: 0.2137,
+      reason:
+        'contain no-speech segment | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 74.17,
+      end: 76.54,
+      score: 0.6235,
+      reason:
+        'contain no-speech segment | reference words without explanations | ',
+    },
+    {
+      start: 76.54,
+      end: 83.88,
+      score: 0.6798,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 83.88,
+      end: 85.32,
+      score: 0.3449,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 85.32,
+      end: 86.75,
+      score: 0.2604,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 86.75,
+      end: 88.22,
+      score: 0.0827,
+      reason:
+        'reference words without explanations | frequent shot changes | numerous visual entites | ',
+    },
+    {
+      start: 88.22,
+      end: 92.39,
+      score: 0.5378,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 92.39,
+      end: 94.59,
+      score: 0.5896,
+      reason:
+        'contain no-speech segment | reference words without explanations | ',
+    },
+    {
+      start: 94.59,
+      end: 97.4,
+      score: 1.0,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 97.4,
+      end: 98.9,
+      score: 0.6019,
+      reason: 'on-screen text not in speech | ',
+    },
+    {
+      start: 98.9,
+      end: 100.53,
+      score: 0.3503,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 100.53,
+      end: 102.4,
+      score: 0.6107,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 102.4,
+      end: 111.75,
+      score: 0.5001,
+      reason: 'reference words without explanations | ',
+    },
+    {
+      start: 111.75,
+      end: 113.58,
+      score: 0.3481,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 113.58,
+      end: 115.38,
+      score: 0.4345,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 115.38,
+      end: 117.58,
+      score: 0.5792,
+      reason: 'specific visual details not covered by speech',
+    },
+    {
+      start: 117.58,
+      end: 139.61,
+      score: 0.2175,
+      reason: 'contain no-speech segment | on-screen text not in speech | ',
+    },
+    {
+      start: 139.61,
+      end: 139.91,
+      score: 0.1597,
+      reason: 'specific visual details not covered by speech',
+    },
+  ],
 }
 
 module.exports = videosRanges
-
-// frequent shot changes | numerous visual entites |
